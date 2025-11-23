8196242d2d2db4a67462636f98bd05ebbf054705
uttejh@gmail.com <uttejh@gmail.com>
2025-11-23 23:26:26 +0100

    add prompt optimization

--- ag_news/prompt_optimization.py
+++ ag_news/prompt_optimization.py
@@ -2,39 +2,20 @@ import dspy
 import datasets
 from typing import Literal
 
-# load AG News dataset
-ag_news = datasets.load_dataset("fancyzhx/ag_news")
 
-# Define the label mapping once
-text_labels = {0: "World", 1: "Sports", 2: "Business", 3: "Sci/Tech"}
+def make_signature(text_labels):
+    class ArticleClassification(dspy.Signature):
+        text: str = dspy.InputField()
+        label: Literal[*text_labels] = dspy.OutputField()
+        confidence: float = dspy.OutputField()
 
-# Define a function to apply the transformation
-def map_label_to_text(sample):
-    sample["text_label"] = text_labels[sample["label"]]
-    return sample
+    return ArticleClassification
 
-ag_news_mapped = ag_news.map(map_label_to_text)
-
-train_set = ag_news_mapped["train"]
-test_set = ag_news_mapped["test"]
-
-train_samples = train_set.shuffle(seed=42).select(range(100))
-test_samples = test_set.shuffle(seed=42).select(range(100))
-
-# write signatures for prompt optimization
-class ArticleClassification(dspy.Signature):
-    """Classify news articles into categories."""
-    text: str = dspy.InputField(desc="News article text.")
-    label: Literal[*text_labels.values()] = dspy.OutputField(desc="Category of the news article.")
-    confidence: float = dspy.OutputField()
-
-classify = dspy.Predict(ArticleClassification)
-LM = dspy.LM(model="openai/Qwen/Qwen3-0.6B", api_base="http://0.0.0.0:8000/v1", api_key="dummy")
-dspy.configure(lm=LM)
 
 def calculate_accuracy(predictions, references):
     correct = sum(p == r for p, r in zip(predictions, references))
     return correct / len(references)
+
 
 def predict_sample(sample):
     result = classify(text=sample["text"])
@@ -42,8 +23,55 @@ def predict_sample(sample):
     sample["predicted_confidence"] = result.confidence
     return sample
 
-results = test_samples.map(predict_sample, num_proc=1)
 
-accuracy = calculate_accuracy(results["predicted_label"], results["text_label"])
-print(f"Accuracy: {accuracy * 100:.2f}%")
-# 71.00% accuracy on 100 test samples without any optimized prompt
+def load_data():
+    # load AG News dataset
+    ag_news = datasets.load_dataset("fancyzhx/ag_news")
+
+    # Define the label mapping once
+    text_labels = {0: "World", 1: "Sports", 2: "Business", 3: "Sci/Tech"}
+
+    ag_news_mapped = ag_news.map(lambda x: {"text_label": text_labels[x["label"]]}, batched=False)
+    train_set = ag_news_mapped["train"]
+    test_set = ag_news_mapped["test"]
+
+    train_samples = train_set.shuffle(seed=42).select(range(100))
+    test_samples = test_set.shuffle(seed=42).select(range(100))
+
+    return train_samples, test_samples
+
+
+def validate_answer(ground_truth, predicted, trace=None):
+    return ground_truth.label == predicted.label
+
+
+def create_examples(dataset):
+    examples = []
+    for sample in dataset:
+        example = dspy.Example(
+            text=sample["text"],
+            label=sample["text_label"]
+        ).with_inputs("text")
+        examples.append(example)
+
+    return examples
+
+if __name__ == "__main__":
+    train_set, test_set = load_data()
+
+    ArticleClassification = make_signature(list(set(train_set["text_label"])))
+    classify = dspy.Predict(ArticleClassification)
+    LM = dspy.LM(model="openai/Qwen/Qwen3-0.6B", api_base="http://0.0.0.0:8000/v1", api_key="dummy")
+    dspy.configure(lm=LM)
+
+    training_examples = create_examples(train_set)
+    testing_examples = create_examples(test_set)
+
+    evaluator = dspy.Evaluate(devset=testing_examples, num_threads=1, display_progress=True, display_table=5)
+    evaluator(classify, metric=validate_answer)
+
+    tp = dspy.MIPROv2(metric=validate_answer, auto="light")
+    optimized_classify = tp.compile(classify, trainset=training_examples, max_labeled_demos=0, max_bootstrapped_demos=0)
+
+    evaluator(optimized_classify, metric=validate_answer)
+    print(dspy.inspect_history())
--- readme.md
+++ readme.md
@@ -3,3 +3,53 @@ Playing with Dspy to automate prompt optimization
 1. **AG news dataset** - 
 The plan is to use the AG news dataset to classify news articles into 4 categories: World, Sports, Business, and Sci/Tech. We will use Dspy to optimize prompts for a language model to improve classification accuracy.
 We use Dspy to create a prompt template and optimize it.
+
+Base prompt before optimization:
+```
+"Your input fields are:
+1. `text` (str): News article text.
+Your output fields are:
+1. `label` (Literal['World', 'Sports', 'Business', 'Sci/Tech']): Category of the news article.
+2. `confidence` (float):
+All interactions will be structured in the following way, with the appropriate values filled in.
+
+[[ ## text ## ]]
+{text}
+
+[[ ## label ## ]]
+
+{label}        # note: the value you produce must exactly match (no extra characters) one of: World; Sports; Business; Sci/Tech
+
+[[ ## confidence ## ]]
+{confidence}        # note: the value you produce must be a single float value
+
+[[ ## completed ## ]]
+In adhering to this structure, your objective is: 
+        Classify news articles into categories."
+```
+
+Accuracy before optimization: 44%
+
+After running Dspy optimization using `MIPROv2`, we obtained the following optimized prompt:
+```
+Your input fields are:
+1. `text` (str):
+Your output fields are:
+1. `label` (Literal['Sports', 'Sci/Tech', 'Business', 'World']): 
+2. `confidence` (float):
+All interactions will be structured in the following way, with the appropriate values filled in.
+
+[[ ## text ## ]]
+{text}
+
+[[ ## label ## ]]
+{label}        # note: the value you produce must exactly match (no extra characters) one of: Sports; Sci/Tech; Business; World
+
+[[ ## confidence ## ]]
+{confidence}        # note: the value you produce must be a single float value
+
+[[ ## completed ## ]]
+In adhering to this structure, your objective is: 
+        Prompt the Language Model to analyze text and predict a label and confidence value based on the text's context and domain.
+```
+Accuracy after optimization: 67%
